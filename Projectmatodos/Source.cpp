5692d252f6b88be469a149d73af8411b97f38158
re <a21110175@live.ceti.mx>
2022-04-18 20:18:16 -0500

    f

---
+++ Projectmatodos/Source.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+#include<iostream>
+
+int variable[3];
+void Solicitar_datos();
+
+void matarasakura();
+
+int  main() {
+
+	Solicitar_datos();
+
+	system("pause");
+	return 0;
+}
+
+
+void Solicitar_datos() {
+
+	int M = 0;//M numero de matrces 
+	std::cout << "Sistema de solucion de metodo de Gauss Seidel\n";
+
+	std::cout << "Ingresa la cantida de matrices \n";
+	std::cin >> M;
+}
+
+void matarasakura() {}
